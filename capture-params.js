187b03205f8a1b118326b4721737e81ad4d057a2
Kokko Tso <kokkotso@gmail.com>
2024-03-14 13:37:19 -0400

    refactor: rename files & functions for consistency

--- capture-params.js
+++ capture-params.js
@@ -1,4 +1,4 @@
-function storeParamsInSession() {
+function captureParams() {
   // Capture & parse url parameters
   const queryString = window.location.search;
 
@@ -28,4 +28,4 @@ function storeParamsInSession() {
   }
 }
 
-Window.onload = storeParamsInSession();
+Window.onload = captureParams();
